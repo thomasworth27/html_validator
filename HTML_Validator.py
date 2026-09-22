9bb0711bbcef2b61cc2b0393c093787ff149ac49
Thomas Worth <tworth77@cmc.edu>
2026-09-22 10:17:01 -0700

    updated validate_html and _extract_tags

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -17,6 +17,20 @@ def validate_html(html):
     # The main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
     # but arbitrary text located between the html tags.
 
+    try:
+        tags = _extract_tags(html)
+    except ValueError:
+        return False
+
+    stack = []
+    for tag in tags:
+        if tag.startswith('</'):
+            if not stack or stack[-1] != tag[2:-1]:
+                return False
+            stack.pop()
+        else:
+            stack.append(tag[1:-1])
+    return len(stack) == 0
 
 def _extract_tags(html):
     '''
@@ -29,3 +43,19 @@ def _extract_tags(html):
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+    tags = []
+    i = 0
+    while True:
+        start = html.find('<', i)
+        if start == -1:
+            break
+        end = html.find('>', start + 1)
+        next_open = html.find('<', start + 1)
+        if end == -1 or (next_open != -1 and next_open < end):
+            raise ValueError('found < without matching >')
+        inner = html[start + 1:end].split()
+        name = inner[0] if inner else ''
+        tags.append('<' + name + '>')
+        i = end + 1
+    return tags
+
